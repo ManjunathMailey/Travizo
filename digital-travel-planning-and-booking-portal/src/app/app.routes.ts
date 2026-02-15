70e77fb6710f7c0d363687ae351507dd94bd7804
ManjunathMailey <manjunathroyalgimail@gmail.com>
2026-02-16 01:10:36 +0530

    setup

---
+++ digital-travel-planning-and-booking-portal/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
+import { DestinationsComponent } from './pages/destinations/destinations';
+import { BookingComponent } from './pages/booking/booking';
+import { ContactComponent } from './pages/contact/contact';
+import { AboutUsComponent } from './pages/about-us/about-us';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'destinations', component: DestinationsComponent },
+  { path: 'booking', component: BookingComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutUsComponent },
+];
---
+++ digital-travel-planning-and-booking-portal/src/app/pages/destinations/destinations.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DestinationsComponent } from './destinations';
+
+describe('DestinationsComponent', () => {
+  let component: DestinationsComponent;
+  let fixture: ComponentFixture<DestinationsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DestinationsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DestinationsComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
